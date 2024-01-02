9379395eb4baf02af32069fcba4624fbdfbf7085
tantien9ee <yanagi.kujou.2002@gmail.com>
2024-01-02 19:47:38 +0700

    update prj structure

---
+++ server.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, request
+import numpy as np
+import re
+import base64
+from PIL import Image
+from imageio import imread
+# from scipy.misc import imread, imresize
+from keras.models import load_model
+import json
